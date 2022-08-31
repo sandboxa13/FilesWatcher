3ad728caed38540df5b547e4e797de0829788733
Dmitry <dmitry.nikishin.84@gmail.com>
2022-09-01 01:19:42 +0300

    added tray and icon

--- src/FileWatcher/App.xaml.cs
+++ src/FileWatcher/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Windows;
 
 namespace FileWatcher
 {
@@ -7,5 +9,17 @@ namespace FileWatcher
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Process proc = Process.GetCurrentProcess();
+            int count = Process.GetProcesses().Where(p =>
+                p.ProcessName == proc.ProcessName).Count();
+
+            if (count > 1)
+            {
+                MessageBox.Show("Приложение уже запущено");
+                App.Current.Shutdown();
+            }
+        }
     }
 }
--- src/FileWatcher/MainWindow.xaml.cs
+++ src/FileWatcher/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using FileWatcher.Logic;
+﻿using System.Windows;
+using System.Windows.Controls;
+using FileWatcher.Logic;
 using FileWatcher.ViewModels;
 
 namespace FileWatcher
@@ -6,25 +8,65 @@ namespace FileWatcher
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow 
+    public partial class MainWindow
     {
+        private readonly System.Windows.Forms.NotifyIcon notifier;
+
         public MainWindow()
         {
+            InitializeComponent();
+
             var permissionsChecker = new PermissionsChecker();
             var permissionsElevator = new PermissionsElevator();
             var filesWatcher = new FilesWatcher();
 
             DataContext = new MainWindowViewModel(permissionsChecker, permissionsElevator, filesWatcher);
 
-            InitializeComponent();
+            notifier = new System.Windows.Forms.NotifyIcon
+            {
+                Icon = new System.Drawing.Icon("../../src/FileWatcher/Resources/icon.ico"),
+                Visible = true
+            };
+            notifier.MouseDown += Notifier_MouseDown;
+        }
+
+        private void Notifier_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                ContextMenu menu = (ContextMenu)this.FindResource("NotifierContextMenu");
+
+                menu.IsOpen = true;
+            }
+        }
+
+        private void Window_StateChanged(object sender, System.EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                this.Hide();
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if(DataContext is MainWindowViewModel viewModel)
+            this.Hide();
+            e.Cancel = true;
+        }
+
+        private void Menu_Show(object sender, RoutedEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Menu_Close(object sender, RoutedEventArgs e)
+        {
+            notifier.MouseDown -= Notifier_MouseDown;
+
+            if (DataContext is MainWindowViewModel viewModel)
             {
                 viewModel.Dispose();
             }
+
+            System.Windows.Application.Current.Shutdown();
         }
     }
 }
